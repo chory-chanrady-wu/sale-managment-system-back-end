21afced6d6970b2f56f64a67e4e9db9dd66a2858
CHORY-Chanrady <chorychanrady168@gmail.com>
2025-09-10 01:34:10 +0700

    .env changed to puplic IP

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ async function getConnection() {
     return await oracledb.getConnection({
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        connectionString: process.env.DB_CONNECTIONSTRING
+        connectString: `${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_SERVICE_NAME}`
     });
 }
 
